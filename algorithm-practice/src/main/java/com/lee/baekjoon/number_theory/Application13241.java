8c8d69bd1cf7a3cd0a6a8560407cb6dad2a68091
이용우 <qhtm008@naver.com>
2025-11-20 00:52:56 +0900

    complete/13241

---
+++ algorithm-practice/src/main/java/com/lee/baekjoon/number_theory/Application13241.java
@@ -0,0 +1,32 @@
+package com.lee.baekjoon.number_theory;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Application13241 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+
+
+        //최소공배수는 a*b에서 최대공약수를 나눈값임!
+
+
+
+            StringTokenizer st=new StringTokenizer(br.readLine());
+            long A=Long.parseLong(st.nextToken());
+            long B=Long.parseLong(st.nextToken());
+            System.out.println(A*B/gcd(A,B));
+
+    }
+
+    public static long gcd(long a,long b){
+        //b가 더 크다고 설정 b>a
+        if(a==0){
+            return b;
+        }else{
+            return gcd(b%a,a);
+        }
+    }
+}
