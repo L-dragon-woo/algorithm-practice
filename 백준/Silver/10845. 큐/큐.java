dc6df284504fd69bddbfecddf779af08f7991d74
L-dragon-woo <qhtm008@naver.com>
2025-11-22 01:28:33 +0900

    [Silver IV] Title: 큐, Time: 424 ms, Memory: 34720 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.java
@@ -0,0 +1,47 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+
+        Deque<Integer> deque=new ArrayDeque<>();
+        Stack<Integer> ans=new Stack<>();
+
+        for (int i=0;i<n;i++){
+            String str=sc.next();
+            if(str.equals("push")){
+                deque.addLast(sc.nextInt());
+            }else if(str.equals("pop")){
+                if(deque.isEmpty()){
+                    ans.push(-1);
+                    continue;
+                }
+                ans.push(deque.removeFirst());
+            }else if(str.equals("size")){
+                ans.push(deque.size());
+            }else if(str.equals("empty")){
+                ans.push(deque.isEmpty() ? 1 : 0);
+            }else if(str.equals("front")){
+                if(deque.isEmpty()){
+                    ans.push(-1);
+                    continue;
+                }
+                ans.push(deque.peekFirst());
+            }else if(str.equals("back")){
+                if(deque.isEmpty()){
+                    ans.push(-1);
+                    continue;
+                }
+                ans.push(deque.peekLast());
+            }
+        }
+
+        for(Integer i:ans){
+            System.out.println(i);
+        }
+    }
+}
