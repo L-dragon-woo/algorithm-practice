b632d62ed47fc6dec7904d71da01107d9d3caf7a
L-dragon-woo <qhtm008@naver.com>
2025-11-22 01:26:48 +0900

    [Bronze III] Title: 세 막대, Time: 100 ms, Memory: 14144 KB -BaekjoonHub

---
+++ 백준/Bronze/14215. 세 막대/세 막대.java
@@ -0,0 +1,40 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st=new StringTokenizer(br.readLine());
+
+        ArrayList<Integer> list=new ArrayList<>();
+
+        while(st.hasMoreTokens()){
+            list.add(Integer.parseInt(st.nextToken()));
+        }
+
+        //제일 긴 변을 구하기
+        int max=Math.max(list.get(2),Math.max(list.get(0),list.get(1)));
+        //나머지 변의 길이 합 구하기
+        int len=list.get(0)+list.get(1)+list.get(2)-max;
+
+        if(max<len){
+            System.out.println(list.get(0)+list.get(1)+list.get(2));
+        }else if(max==len){
+            System.out.println(max*2-1);
+        }else if(max>len){
+            System.out.println(len*2-1);
+        }
+
+
+
+
+
+    }
+
+}
