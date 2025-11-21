2ee3056198f69b16dbacaa1cc1af31c1ac77d424
L-dragon-woo <qhtm008@naver.com>
2025-11-22 01:29:59 +0900

    [Bronze II] Title: 커트라인, Time: 116 ms, Memory: 14452 KB -BaekjoonHub

---
+++ 백준/Bronze/25305. 커트라인/커트라인.java
@@ -0,0 +1,27 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st=new StringTokenizer(br.readLine());
+        int N=Integer.parseInt(st.nextToken());
+        int k=Integer.parseInt(st.nextToken());
+
+        ArrayList<Integer> list=new ArrayList<>();
+        StringTokenizer st1=new StringTokenizer(br.readLine());
+
+        while(st1.hasMoreTokens()){
+            list.add(Integer.parseInt(st1.nextToken()));
+        }
+
+        list.sort((a,b)->(b-a));
+        System.out.println(list.get(k-1));
+
+    }
+}
