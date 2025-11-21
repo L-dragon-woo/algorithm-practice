6c7e071a930f397776b52ed5ca9febb8974bd88b
L-dragon-woo <qhtm008@naver.com>
2025-11-22 01:28:50 +0900

    [Silver IV] Title: 대칭 차집합, Time: 724 ms, Memory: 83152 KB -BaekjoonHub

---
+++ 백준/Silver/1269. 대칭 차집합/대칭 차집합.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        //N,M 받기
+        StringTokenizer st=new StringTokenizer(br.readLine());
+        int N=Integer.parseInt(st.nextToken());
+        int M=Integer.parseInt(st.nextToken());
+
+        HashSet<Integer>set=new HashSet<>();
+
+        StringTokenizer st1=new StringTokenizer(br.readLine());
+
+        //처음 집합 저장
+        while(st1.hasMoreTokens()){
+            set.add(Integer.parseInt(st1.nextToken()));
+        }
+
+        StringTokenizer st2=new StringTokenizer(br.readLine());
+
+        while(st2.hasMoreTokens()){
+            int a=Integer.parseInt(st2.nextToken());
+            if(set.contains(a)){
+                set.remove(a);
+            }else{
+                set.add(a);
+            }
+        }
+        System.out.println(set.size());
+    }
+}
