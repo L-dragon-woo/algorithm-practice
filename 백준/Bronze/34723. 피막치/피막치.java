5be820d969612194a8976d03eca1c6e916dfb05d
L-dragon-woo <qhtm008@naver.com>
2025-11-22 01:11:48 +0900

    [Bronze III] Title: 피막치, Time: 180 ms, Memory: 17592 KB -BaekjoonHub

---
+++ 백준/Bronze/34723. 피막치/피막치.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int P=sc.nextInt();
+        int M=sc.nextInt();
+        int C=sc.nextInt();
+        int X=sc.nextInt();
+
+        long min=1000000001l;
+        //피자 고르는 경우
+        for(int i=1; i<=P;i++){
+            //막국수 고르는 경우
+            for(int j=1; j<=M;j++){
+                //치킨 고르는 경우
+                for(int k=1; k<=C;k++){
+                    min=Math.min(min,Math.abs((i+j)*(j+k)-X));
+                }
+            }
+        }
+        System.out.println(min);
+    }
+}
