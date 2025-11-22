1eab137cd8281f41a5f6aa42c77fdf0a7842e3a9
L-dragon-woo <qhtm008@naver.com>
2025-11-22 23:56:04 +0900

    [Silver IV] Title: 제로, Time: 212 ms, Memory: 23204 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.java
@@ -0,0 +1,27 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        int K=Integer.parseInt(br.readLine());
+        Stack<Integer> stack=new Stack<>();
+
+        int cnt=0;
+        for(int i=0;i<K;i++){
+            int N=Integer.parseInt(br.readLine());
+            if(N==0){
+                if(!stack.isEmpty()){
+                    cnt-=stack.pop();
+                }
+            }else{
+                stack.push(N);
+                cnt+=N;
+            }
+        }
+        System.out.println(cnt);
+    }
+}
