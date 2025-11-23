2fae0949ba7524d6fc918d0937905c3c8411b4a1
L-dragon-woo <qhtm008@naver.com>
2025-11-23 20:40:45 +0900

    [Silver III] Title: 도키도키 간식드리미, Time: 112 ms, Memory: 14680 KB -BaekjoonHub

---
+++ 백준/Silver/12789. 도키도키 간식드리미/도키도키 간식드리미.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        int N=Integer.parseInt(br.readLine());
+        Deque<Integer> deque=new java.util.ArrayDeque<>();
+
+        StringTokenizer st=new StringTokenizer(br.readLine());
+
+        String ans="Nice";
+        //입력받아오기
+        while(st.hasMoreTokens()){
+            deque.offerLast(Integer.parseInt(st.nextToken()));
+        }
+
+        //순서 할당 스택
+        Stack<Integer> stack=new Stack<>();
+
+        int temp=1;
+        while(true){
+            if(temp==N)break;
+
+            if(!deque.isEmpty()&&deque.peekFirst()==temp){
+                deque.pollFirst();
+                temp++;
+            }else if(!stack.isEmpty() && stack.peek()==temp){
+                stack.pop();
+                temp++;
+            }//덱에도 스택에도 없으면 deque에 있는걸 stack에계속해서 보냄
+            else{
+                if(!stack.isEmpty()&&stack.peek()<deque.peekFirst()){
+                    ans="Sad";
+                    break;
+                }else{
+                    stack.push(deque.pollFirst());
+                }
+            }
+        }
+        System.out.println(ans);
+    }
+}
+
