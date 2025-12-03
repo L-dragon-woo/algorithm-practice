e14801a7b31823b63e6b4919aebc575fb026d79c
L-dragon-woo <qhtm008@naver.com>
2025-12-04 01:46:13 +0900

    [Silver V] Title: 팩토리얼 0의 개수, Time: 124 ms, Memory: 14980 KB -BaekjoonHub

---
+++ 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        BigInteger a=new BigInteger(br.readLine());
+        BigInteger b=factorial(a);
+        BigInteger c=BigInteger.TEN;
+        int cnt=0;
+        while(true){
+            if(b.mod(c).equals(BigInteger.ZERO)){
+                cnt++;
+                c=c.multiply(BigInteger.valueOf(10));
+            }else{
+                System.out.println(cnt);
+                break;
+            }
+        }
+
+    }
+    public static BigInteger factorial(BigInteger n){
+        if(n.equals(BigInteger.ZERO)){
+            return BigInteger.ONE;
+        }else{
+            return n.multiply(factorial(n.subtract(BigInteger.ONE)));
+        }
+    }
+}
