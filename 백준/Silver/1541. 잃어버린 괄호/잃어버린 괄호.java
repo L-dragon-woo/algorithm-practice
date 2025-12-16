0895a9a4dddc185eff3d3dd34e64084836ff541e
L-dragon-woo <qhtm008@naver.com>
2025-12-16 15:14:57 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 104 ms, Memory: 14236 KB -BaekjoonHub

---
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        // '-' 로 분리
+        String[]arr=br.readLine().split("-");
+       //55  55+40
+        // `+`로 분리
+        for(int i=0;i<arr.length;i++){
+            StringTokenizer st=new StringTokenizer(arr[i],"+");
+            int sum=0;
+            while(st.hasMoreTokens()){
+                sum+=Integer.parseInt(st.nextToken());
+            }
+            arr[i]=String.valueOf(sum);
+        }
+
+
+        int a=Integer.parseInt(arr[0]);
+        for(int i=1;i<arr.length;i++){
+            a-=Integer.parseInt(arr[i]);
+        }
+        System.out.println(a);
+
+    }
+
+}
