cdf743a4096741886d29cf814183f73b066eb179
L-dragon-woo <qhtm008@naver.com>
2026-02-13 17:26:31 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.05 ms, Memory: 54.5 MB -BaekjoonHub

--- 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.java
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.java
@@ -2,14 +2,10 @@ import java.util.*;
 
 public class Solution {
     public int solution(int n) {
-        int ans=0;
-        String binary=Integer.toBinaryString(n);
-        for(int i=0;i<binary.length();i++){
-            if(binary.charAt(i)=='1'){
-                ans++;
-            }
-        }
-        
+        int ans = 0;
+
+        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+        System.out.println("Hello Java");
 
         return ans;
     }
