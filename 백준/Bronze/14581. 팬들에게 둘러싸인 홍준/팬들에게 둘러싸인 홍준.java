02da68ec6e6785036d1ebb63b851c6ddf847fc6c
L-dragon-woo <qhtm008@naver.com>
2026-02-16 07:54:57 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 112 ms, Memory: 15856 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+
+        System.out.println(":fan::fan::fan:");
+        System.out.println(":fan::" + str + "::fan:");
+        System.out.println(":fan::fan::fan:");
+        
+    }
+}
