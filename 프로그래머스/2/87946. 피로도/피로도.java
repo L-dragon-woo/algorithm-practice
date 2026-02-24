26142aca0c1734315ae6586d597ae4c154eceec3
L-dragon-woo <qhtm008@naver.com>
2026-02-24 23:44:00 +0900

    Implement fatigue management in dungeon exploration

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,13 +1,46 @@
 import java.util.*;
+
 class Solution {
+
+    static class State {
+        int fatigue;
+        int count;
+        boolean[] visited;
+
+        State(int fatigue, int count, boolean[] visited) {
+            this.fatigue = fatigue;
+            this.count = count;
+            this.visited = visited;
+        }
+    }
+
     public int solution(int k, int[][] dungeons) {
-        int answer = -1;
-        
-        Arrays.sort(dungeons,(a,b)->a[0]-b[0]);
-        
-        System.out.println(Arrays.deepToString(dungeons));
-        
-        
-        return answer;
+
+        int max = 0;
+        Stack<State> stack = new Stack<>();
+
+        stack.push(new State(k, 0, new boolean[dungeons.length]));
+
+        while (!stack.isEmpty()) {
+
+            State cur = stack.pop();
+            max = Math.max(max, cur.count);
+
+            for (int i = 0; i < dungeons.length; i++) {
+                if (!cur.visited[i] && cur.fatigue >= dungeons[i][0]) {
+
+                    boolean[] newVisited = cur.visited.clone();
+                    newVisited[i] = true;
+
+                    stack.push(new State(
+                            cur.fatigue - dungeons[i][1],
+                            cur.count + 1,
+                            newVisited
+                    ));
+                }
+            }
+        }
+
+        return max;
     }
 }
